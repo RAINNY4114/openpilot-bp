4fb57cc08a361923a1abf4c79bb605d35008b7b5
RAINNY4114 <152569703+RAINNY4114@users.noreply.github.com>
2025-01-23 13:06:40 +0800

    Update carstate.py

--- selfdrive/car/ford/carstate.py
+++ selfdrive/car/ford/carstate.py
@@ -112,12 +112,12 @@ class CarState(CarStateBase):
 
     # gear
     if self.CP.transmissionType == TransmissionType.automatic:
-         if self.CP.flags & FordFlags.CANFD:
-           gear = self.shifter_values.get(cp.vl["Gear_Shift_by_Wire_FD1"]["TrnRng_D_RqGsm"])
-         elif self.CP.flags & FordFlags.ALT_STEER_ANGLE:
-           if (cp.vl["TransGearData"]["GearLvrPos_D_Actl"] in (3, 4, 5)):
-         ret.gearShifter = GearShifter.drive
-       elif (cp.vl["TransGearData"]["GearLvrPos_D_Actl"] == 1):
+      if self.CP.flags & FordFlags.CANFD:
+        gear = self.shifter_values.get(cp.vl["Gear_Shift_by_Wire_FD1"]["TrnRng_D_RqGsm"])
+      elif self.CP.flags & FordFlags.ALT_STEER_ANGLE:
+        if (cp.vl["TransGearData"]["GearLvrPos_D_Actl"] in (3, 4, 5)):
+        ret.gearShifter = GearShifter.drive
+      elif (cp.vl["TransGearData"]["GearLvrPos_D_Actl"] == 1):
         ret.gearShifter = GearShifter.reverse
     elif self.CP.transmissionType == TransmissionType.manual:
       ret.clutchPressed = cp.vl["Engine_Clutch_Data"]["CluPdlPos_Pc_Meas"] > 0
